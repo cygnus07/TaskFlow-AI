5a85af73ac98701b438170f7bb8182cb3e49cbad
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-07-18 11:58:29 +0530

    set up express app with middlewares

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express, {Application, Request, Response, NextFunction} from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import {config} from './config/index.js'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import morgan from 'morgan'
+
+
+
+export const createApp = (): Application => {
+    const app = express()
+    app.use(helmet())
+    app.use(cors(config.cors))
+    app.use(express.json({ limit: '10mb'}))
+    app.use(express.urlencoded({
+        extended: true,
+        limit: '10mb'
+    }))
+    app.use(cookieParser())
+    app.use(compression())
+
+    if(config.env != 'test'){
+        app.use(morgan('dev'))
+    }
+
+    app.get('/health', (_req: Request, res: Response) => {
+        res.status(200).json({
+            status: 'ok',
+            timeStamp: new Date().toISOString(),
+            environment: config.env,
+        })
+    }) 
+
+    // api routes
+
+    app.use((err: Error, _req: Request, res: Response, _next: NextFunction ) => {
+        console.error(err.stack)
+        res.status(500).json({
+            status: 'error',
+            message: config.env === 'production'
+            ? "Internal Server Error" 
+            : err.message
+        })
+    })
+
+    return app
+}
