6e5f7c006febf114d89f30883b99bbb0c56ad4e0
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-07-22 09:05:06 +0530

    Add initial routes for authentication and health check

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import authRoutes from './auth.routes.js'
+
+const router = Router()
+
+router.use('/auth', authRoutes)
+
+router.get('/health', (_req,res) => {
+    res.json({
+        success: true,
+        message: 'Api is running',
+        timeStamp: new Date().toISOString(),
+    })
+})
+
