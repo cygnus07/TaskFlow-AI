0b9124b233164c73b10f49c27282370a3c1eeb6a
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-07-28 23:31:07 +0530

    Add task routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from './auth.routes.js'
 import projectRoutes from './project.routes.js'
+import taskRoutes from './task.routes.js'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/projects', projectRoutes)
+router.use('/', taskRoutes)
 
 
 router.get('/health', (_req,res) => {
