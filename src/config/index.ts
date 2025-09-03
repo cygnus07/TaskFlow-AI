cc802e0050a0f463bd4c858ef460b6c3ff52b82d
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-09-03 15:40:22 +0530

    Update CACHE_TTL coercion to use number method

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ const envSchema = z.object({
     OPENAI_MODEL: z.string().default('gpt-4-turbo-preview'),
     AI_FEATURES_ENABLED: z.string().transform( val => val === 'true').default('false'),
     REDIS_URL: z.string().default('redis://localhost:6379'),
-    CACHE_TTL: z.coerce.transform(Number).default('3600')
+    CACHE_TTL: z.coerce.number().default(3600)
 
 })
 
