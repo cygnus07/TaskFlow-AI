06b928db127ce49f281a065a487967177c3d8ba4
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-09-06 19:45:02 +0530

    Add Jest, Supertest, Faker, Redis & MongoDB test setup

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+    '!src/types/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testTimeout: 30000,
+};
---
+++ tests/setup.ts
@@ -0,0 +1,49 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
+import RedisMemoryServer from "redis-memory-server"
+import { redisClient } from '../src/config/redis.js'
+
+let mongoServer: MongoMemoryServer
+let redisServer: RedisMemoryServer
+
+beforeAll(async  () => {
+    mongoServer = await MongoMemoryServer.create()
+    const mongoUri = mongoServer.getUri()
+
+    redisServer = new RedisMemoryServer()
+    const redisHost = await redisServer.getHost()
+    const redisPort = await redisServer.getPort()
+
+    process.env.MONGO_URI = mongoUri
+    process.env.REDIS_URL = `redis://${redisHost}:${redisPort}`
+    process.env.JWT_SECRET = 'test-secret-key-for-testing-only'
+    process.env.NODE_ENV = 'test'
+
+    await mongoose.connect(mongoUri)
+    await redisClient.connect()
+
+})
+
+afterEach(async () => {
+    const collections = mongoose.connection.collections
+    for(const key in collections){
+        await collections[key].deleteMany({})
+    }
+
+    const client = redisClient.getClient()
+    await client.flushall()
+})
+
+afterAll(async () => {
+    await mongoose.disconnect()
+    await redisClient.disconnect()
+    await mongoServer.stop()
+    await redisServer.stop()
+})
+
+global.consoe = {
+    ...console,
+    log: jest.fn(),
+    error: jest.fn(),
+    warn: jest.fn()
+}
